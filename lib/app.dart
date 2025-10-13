c2d2d4e46a1fd3c04991c3ecd8ab1740e8a6b820
Noureddine <engnoureddinemohamed@gmail.com>
2025-10-13 16:32:22 +0300

    rename app

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Eventaty extends StatelessWidget {
+  const Eventaty({super.key});
 
   @override
   Widget build(BuildContext context) {
