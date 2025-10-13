5a608b0d1a69214408a776b9c858c2480c2ca148
Moamen771 <memoaser86@gmail.com>
2025-10-13 19:16:44 +0300

    feat: Add color manager

---
+++ lib/core/utlis/color_manager.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  // Typography Colors
+  static const Color typBlack = Color(0xFF120D26);
+  static const Color typBlackVariant = Color(0xFF120D26);
+  static const Color typGray1 = Color(0xFF888693); // #888693
+  static const Color typGray2 = Color(0xFFB8B6BE); // #B8B6BE
+  static const Color typGray3 = Color(0xFFD0CFD4); // #D0CFD4
+
+  // Accent Colors
+  static const Color accentRed = Color(0xFFF0635A); // #F0635A
+  static const Color accentOrange = Color(0xFF39B54A); // #39B54A
+  static const Color accentGreen = Color(0xFFF0635A); // #F0635A
+  static const Color accentDarkCyan = Color(0xFF46CDFB); // #46CDFB
+  static const Color accentPurple = Color(0xFFF0635A); // #F0635A
+  static const Color accentYellow = Color(0xFFFDC400); // #FDC400
+
+  // Background Colors
+  static const Color backgroundBlack1 = Color(0xFF39B54A); // #39B54A
+  static const Color backgroundBlack2 = Color(0xFF39B54A); // #39B54A
+  static const Color backgroundGray = Color(0xFFE6E6E6); // #E6E6E6
+}
